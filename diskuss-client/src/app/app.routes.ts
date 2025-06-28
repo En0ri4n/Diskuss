f6b6ea0b448d251047441629e98314429d63e62e
Enorian <enorian.rajo@gmail.com>
2025-06-28 17:04:12 +0200

    Add home page

--- diskuss-client/src/app/app.routes.ts
+++ diskuss-client/src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   // Add more routes later
 ];
