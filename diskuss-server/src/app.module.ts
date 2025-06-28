08f2e7d731162e76de872f70d22de6260d886ab3
Enorian <enorian.rajo@gmail.com>
2025-06-28 11:56:57 +0200

    Add various packages

--- diskuss-server/src/app.module.ts
+++ diskuss-server/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {User, UserSchema} from "./users/user.schema";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Message, MessageSchema} from "./messages/message.schema";
+import {Chat, ChatSchema} from "./chats/chat.schema";
+import {Notification, NotificationSchema} from "./notifications/notification.schema";
+import {AuthModule} from "./auth/auth.module";
+import {UserModule} from "./users/user.module";
+import {ChatModule} from "./chats/chat.module";
+import {MessageModule} from "./messages/message.module";
 
 @Module({
-  imports: [],
+  imports: [
+      AuthModule,
+      UserModule,
+      ChatModule,
+      MessageModule,
+      ConfigModule.forRoot({ isGlobal: true }),
+      MongooseModule.forRoot('mongodb://home.enorian.dev/diskuss-db'),
+      MongooseModule.forFeature([
+        { name: User.name, schema: UserSchema },
+        { name: Message.name, schema: MessageSchema },
+        { name: Chat.name, schema: ChatSchema },
+        { name: Notification.name, schema: NotificationSchema },
+      ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
