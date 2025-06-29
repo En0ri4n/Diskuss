2122dac3a530d3d42d633fe6fe6cdd7b1c0d2102
Enorian <enorian.rajo@gmail.com>
2025-06-29 12:45:13 +0200

    Remove unused import

--- diskuss-client/src/app/services/users/user.service.ts
+++ diskuss-client/src/app/services/users/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject, } from 'rxjs';
 import {config} from '../../config';
 import {AuthResponse} from '../auth/dto/auth-response.dto';
 
