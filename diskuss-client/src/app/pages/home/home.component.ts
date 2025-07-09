d7a2d5267a97debdc1d67df74f295d05c9989472
Enorian <enorian.rajo@gmail.com>
2025-07-10 00:35:07 +0200

    Add header to home page

--- diskuss-client/src/app/pages/home/home.component.ts
+++ diskuss-client/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ChatComponent} from '../../components/chat/chat.component';
 import {RouterLink} from '@angular/router';
+import {HeaderComponent} from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     ChatComponent,
-    RouterLink
+    RouterLink,
+    HeaderComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
